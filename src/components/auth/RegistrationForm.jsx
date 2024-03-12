0d3216f8cf8efb077b6a96dde0ccc7c062721d96
nildoria <sabbir.ahmed87@gmail.com>
2024-03-13 01:19:50 +0600

    Registration form manage and registration process manage

---
+++ src/components/auth/RegistrationForm.jsx
@@ -0,0 +1,96 @@
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import Field from "../common/Field";
+
+import axios from "axios";
+
+const RegistrationForm = () => {
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm();
+
+  const submitForm = async (formData) => {
+    console.log(formData);
+    try {
+      let response = await axios.post(
+        `${import.meta.env.VITE_SERVER_BASE_URL}/auth/register`,
+        formData
+      );
+
+      if (response.status === 201) {
+        navigate("/login");
+      }
+    } catch (error) {
+      console.log(error);
+      setError("root.random", {
+        type: "random",
+        message: `Something went wrong: ${error.message}`,
+      });
+    }
+  };
+
+  return (
+    <form
+      className="border-b border-[#3F3F3F] pb-10 lg:pb-[30px]"
+      onSubmit={handleSubmit(submitForm)}
+    >
+      <Field label="First Name" error={errors.firstName}>
+        <input
+          {...register("firstName", { required: "First Name is required" })}
+          className={`auth-input ${errors.firstName ? "border-red-500" : ""}`}
+          name="firstName"
+          type="text"
+          id="firstName"
+        />
+      </Field>
+      <Field label="Last Name" error={errors.lastName}>
+        <input
+          {...register("lastName")}
+          className={`auth-input ${errors.lastName ? "border-red-500" : ""}`}
+          name="lastName"
+          type="text"
+          id="lastName"
+        />
+      </Field>
+      <Field label="Email" error={errors.email}>
+        <input
+          {...register("email", { required: "Email is required" })}
+          className={`auth-input ${errors.email ? "border-red-500" : ""}`}
+          name="email"
+          type="email"
+          id="email"
+        />
+      </Field>
+      <Field label="Password" error={errors.password}>
+        <input
+          {...register("password", {
+            required: "Password is required",
+            minLength: {
+              value: 8,
+              message: "Password must be at least 8 characters",
+            },
+          })}
+          className={`auth-input ${errors.password ? "border-red-500" : ""}`}
+          name="password"
+          type="password"
+          id="password"
+        />
+      </Field>
+      <p>{errors?.root?.random?.message}</p>
+      <Field>
+        <button
+          className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90"
+          type="submit"
+        >
+          Register
+        </button>
+      </Field>
+    </form>
+  );
+};
+
+export default RegistrationForm;
